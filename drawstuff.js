7de6fa0afe20b22f48e75ecf63d8ca42b9e90e9d
CJ Christiance <cj@LAPTOP-LEGION.lan>
2026-08-21 18:02:48 -0400

    Dvd bounce

--- drawstuff.js
+++ drawstuff.js
@@ -40,6 +40,14 @@ class Color {
     } // end Color change method
 } // end color class
 
+class Box {
+  constructor(xPos, yPos, xVel, yVel, w, h, color) {
+    this.xPos = xPos; this.yPos = yPos;
+    this.xVel = xVel; this.yVel = yVel;
+    this.w = w; this.h = h;
+    this.color = color;
+  }
+}
 
 /* utility functions */
 
@@ -78,12 +86,52 @@ function main() {
     var imagedata = context.createImageData(w,h);
  
     // Draw a rectangle with pixels
-    var c = new Color(91,206,250,255); // the color at the pixel: black opaque
-    for (var x=50; x<100; x++) 
-        for (var y=50; y<500; y++) {
-            drawPixel(imagedata,x,y,c);
-            // console.log("draw at " +x+ " " +y);
-        }
-    
-    context.putImageData(imagedata, 0, 0); // display the image in the context
+    var r = 91;
+    var g = 206;
+    var b = 250;
+
+    var c = new Color(r,g,b,255); // the color at the pixel: black opaque
+    var box = new Box(150, 200, 3, 3, 50, 50, c);
+    setInterval(() => {
+      dvdBounce(box, canvas, context, imagedata)
+    }, 16);
+}
+
+function dvdBounce(b, canvas, context, imagedata) {
+  imagedata.data.fill(0); // clear every pixel from previous frame
+
+  for (var x=b.xPos; x<(b.xPos + b.w); x++) {
+    for (var y=b.yPos; y<(b.yPos + b.h); y++) {
+      drawPixel(imagedata,x,y,b.color);
+      // console.log("draw at " +x+ " " +y);
+     }
+  }
+
+  var red = Math.floor(Math.random() * 255);
+  var gre = Math.floor(Math.random() * 255);
+  var blu = Math.floor(Math.random() * 255);
+  if (b.xPos <= 0) { // left
+    b.xPos = 0;
+    b.xVel = Math.floor(Math.random() * 5) + 3;
+    b.color.r = red; b.color.gre = gre; b.color.b = blu;
+  } else if (b.xPos + b.w >= canvas.width) { // right
+    b.xPos = canvas.width - b.w;
+    b.xVel = -(Math.floor(Math.random() * 5) + 3);
+    b.color.r = red; b.color.gre = gre; b.color.b = blu;
+  }
+
+  if (b.yPos <= 0) { // top
+    b.yPos = 0;
+    b.yVel = Math.floor(Math.random() * 5) + 3;
+    b.color.r = red; b.color.gre = gre; b.color.b = blu;
+  } else if (b.yPos + b.h >= canvas.height) { // bottom
+    b.yPos = canvas.height - b.h;
+    b.yVel = -(Math.floor(Math.random() * 5) + 3);
+    b.color.r = red; b.color.gre = gre; b.color.b = blu;
+  }
+
+  b.xPos += b.xVel;
+  b.yPos += b.yVel;
+
+  context.putImageData(imagedata, 0, 0); // display the image in the context
 }
